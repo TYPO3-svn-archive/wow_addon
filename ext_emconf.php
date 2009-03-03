b19d2c17ccd172bf0a8695164021b4b7b8676db2
jobe <jobe@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-03 06:51:12 +0000

    inital import
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/wow_addon/trunk@17429 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "wow_addon"
+#
+# Auto generated 04-10-2008 09:40
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'World of Warcraft - Addon Service',
+	'description' => 'Manage and provide addons for World of Warcraft',
+	'category' => 'plugin',
+	'author' => 'Jobe',
+	'author_email' => 'jobe@jobesoft.de',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'experimental',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"b392";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"5cd9";s:14:"ext_tables.php";s:4:"e0a8";s:13:"locallang.xml";s:4:"018c";s:16:"locallang_db.xml";s:4:"0f12";s:19:"doc/wizard_form.dat";s:4:"7882";s:20:"doc/wizard_form.html";s:4:"3ada";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:29:"pi1/class.tx_wowaddon_pi1.php";s:4:"7d71";s:37:"pi1/class.tx_wowaddon_pi1_wizicon.php";s:4:"ae5b";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"6a57";s:24:"pi1/static/editorcfg.txt";s:4:"05bd";}',
+);
+
+?>
