5f3341ae7742f891e266296420b951376fe758da
jobe <jobe@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-05-09 07:48:08 +0000

    unify category & dependencies
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/wow_addon/trunk@33058 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,9 +11,9 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'World of Warcraft - Addon Service',
+	'title' => 'World of Warcraft - Addons',
 	'description' => 'Manage and provide addons for World of Warcraft',
-	'category' => 'plugin',
+	'category' => 'World of Warcraft',
 	'author' => 'Jobe',
 	'author_email' => 'jobe@jobesoft.de',
 	'shy' => '',
@@ -32,6 +32,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.0',
 	'constraints' => array(
 		'depends' => array(
+			'cms' => '',
+			'typo3' => '4.3.2',
+			'php' => '5.0',
+			'wow_armory' => '0.0.0',
 		),
 		'conflicts' => array(
 		),
